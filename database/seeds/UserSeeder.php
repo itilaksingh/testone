8da6d2cdaeea01fd0d372a6838b27ccce7fd1864
tilak singh <t.singh3713@gmail.com>
2022-05-29 00:16:11 +0530

    new commit

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,7 +12,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $users = factory(User::class, 100)->make()->toArray();
+        $users = factory(User::class, 250)->make()->toArray();
 
         foreach ($users as $user) {
             App\User::create($user);
